5f0a6128346075258d23e1212a0747d79437ead7
gigaherz <gigaherz@gmail.com>
2023-08-26 10:17:26 +0200

    Backport from 1.20.1: Use eyes render type instead of entityTranslucent. Fixes the eyes not being visible enough while in the darkness effect, and with certain shaders.

--- src/main/java/dev/gigaherz/eyes/client/EyesRenderer.java
+++ src/main/java/dev/gigaherz/eyes/client/EyesRenderer.java
@@ -19,7 +19,7 @@ import net.minecraft.world.level.LightLayer;
 public class EyesRenderer extends EntityRenderer<EyesEntity>
 {
     private static final ResourceLocation TEXTURE = EyesInTheDarkness.location("textures/entity/eyes1.png");
-    private final RenderType renderType = RenderType.entityTranslucent(TEXTURE);
+    private final RenderType renderType = RenderType.eyes(TEXTURE);
 
     public EyesRenderer(EntityRendererProvider.Context context)
     {
@@ -27,7 +27,7 @@ public class EyesRenderer extends EntityRenderer<EyesEntity>
     }
 
     @Override
-    public void render(EyesEntity entity, float entityYaw, float partialTicks, PoseStack matrixStack, MultiBufferSource bufferIn, int packedLightIn)
+    public void render(EyesEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightmapCoords)
     {
         BlockPos position = entity.getBlockPosEyes();
 
@@ -46,9 +46,9 @@ public class EyesRenderer extends EntityRenderer<EyesEntity>
         if (mixAlpha <= 0)
             return;
 
-        matrixStack.pushPose();
-        matrixStack.translate(0, entity.getEyeHeight(), 0);
-        matrixStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+        poseStack.pushPose();
+        poseStack.translate(0, entity.getEyeHeight(), 0);
+        poseStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
 
         float aggro = entity.getAggroLevel();
 
@@ -63,9 +63,8 @@ public class EyesRenderer extends EntityRenderer<EyesEntity>
         float hoff = getBlinkState(entity, partialTicks, th);
 
         int packedOverlayCoords = OverlayTexture.NO_OVERLAY;
-        int packedLightmapCoords = 0x00F000F0;
         VertexConsumer buffer = bufferIn.getBuffer(renderType);
-        Matrix4f matrix = matrixStack.last().pose();
+        Matrix4f matrix = poseStack.last().pose();
         buffer.vertex(matrix, -w, -h, 0)
                 .color(1.0F, aggroColorAdjust, aggroColorAdjust, mixAlpha)
                 .uv(0, hoff + th)
@@ -95,9 +94,9 @@ public class EyesRenderer extends EntityRenderer<EyesEntity>
                 .normal(0, 0, 1)
                 .endVertex();
 
-        matrixStack.popPose();
+        poseStack.popPose();
 
-        super.render(entity, entityYaw, partialTicks, matrixStack, bufferIn, packedLightIn);
+        super.render(entity, entityYaw, partialTicks, poseStack, bufferIn, packedLightmapCoords);
     }
 
     private float getBlinkState(EyesEntity entity, float partialTicks, float th)
